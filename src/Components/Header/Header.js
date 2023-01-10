9cc0db6d7ba49a43b9152b36c5fb91ac13b4ca4e
Kajalgupta1011 <guptakajal040917@gmail.com>
2023-01-11 02:53:49 +0530

    updtae header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Header.css'
 import logo from '../../assets/logo.svg'
 function Header() {
+  
   return (
     <header className='NavBar'>
         <div className='menu'>
@@ -33,5 +34,4 @@ function Header() {
     </header>
   )
 }
-
 export default Header
