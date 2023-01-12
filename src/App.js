72c35a8cc7bde7e669f44209b78afd1cac308b93
Kajalgupta1011 <guptakajal040917@gmail.com>
2023-01-13 00:25:50 +0530

    update banner5 components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import Banner1 from "./Components/Banner/Banner1";
-import Banner2 from "./Components/Banner/Banner2";
-import Banner3 from "./Components/Banner/Banner3";
-import Banner4 from "./Components/Banner/Banner4";
+// import Banner1 from "./Components/Banner/Banner1";
+// import Banner2 from "./Components/Banner/Banner2";
+// import Banner3 from "./Components/Banner/Banner3";
+// import Banner4 from "./Components/Banner/Banner4";
+import Banner5 from "./Components/Banner/Banner5";
 import Header from "./Components/Header/Header";
 
 // import logo from './logo.svg';
@@ -12,7 +13,8 @@ function App() {
       {/* <Banner1/>
       <Banner2/>
       <Banner3/> */}
-      <Banner4/>
+      {/* <Banner4/> */}
+      <Banner5/>
     </div>
   );
 }
--- src/Components/Banner/Banner5.js
+++ src/Components/Banner/Banner5.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Banner5.css'
+import banner5img from '../../assets/banner5img.jpeg'
 function Banner5() {
   return (
     <div className='banner5-Container'>
         <div className="banner5">
+            <div className="banner5-part1">
+                {/* <div className="part1-inner"> */}
+                  <img className='part1-inner' src={banner5img} alt="" />
+                {/* </div> */}
+                <div className="part1-wall"></div>
+                <div className="part1-2wall"></div>
+            </div>
+            <div className="banner5-part2">
+              <div className="banner5-txt">
+                <h1>Robust and malleable</h1>
+                <ul>
+                  <li>Create a compelling website.</li>
+                  <li>Use custom codes like no other.</li>
+                  <li>Designs a swift, streamlined design that is clean, minimalistic, futuristic, and has an unlimited gallery store.</li>
+                  <li>Instant setup - create a website in just minutes.</li>
+                  <li>Get a custom Email ID.</li>
+                  <li>Have the complete process taken care of for you.</li>
+                </ul>
+                <button className='banner5-btn'>Learn More</button>
+              </div>
+            </div>
             
         </div>
     </div>
